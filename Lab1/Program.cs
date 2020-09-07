ad27f640f76b4abe32e7ff7667e201bd6677926a
Eva-Lena Stegmann <eva.lena.stegmann@iths.se>
2020-09-07 15:58:48 +0200

    Added the rest of the files

---
+++ Lab1/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+
+namespace Lab1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = "";
+            if (args.Length > 0)
+            {
+                input = args[0];
+            }
+
+            ConsoleColor currentForeground = Console.ForegroundColor;
+            ConsoleColor currentBackground = Console.BackgroundColor;
+            Console.BackgroundColor = ConsoleColor.Black;
+            Console.ForegroundColor = ConsoleColor.White;
+
+            long sum = 0;
+
+            for (int currentPos = 0; currentPos < input.Length; currentPos++)
+            {
+                int alsoFoundAt = input.IndexOf(input[currentPos], currentPos + 1);
+                if (alsoFoundAt > currentPos && long.TryParse(input.Substring(currentPos, alsoFoundAt - currentPos + 1), out long foundNumber) && input[currentPos] != ' ')
+                {
+                    Console.Write(input.Substring(0, currentPos));
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.Write(foundNumber);
+                    Console.ForegroundColor = ConsoleColor.White;
+                    Console.WriteLine(input.Substring(alsoFoundAt + 1));
+
+                    sum += foundNumber;
+                }              
+            }
+
+            Console.WriteLine("");
+            Console.WriteLine($"Total = {sum}");
+
+            Console.ForegroundColor = currentForeground;
+            Console.BackgroundColor = currentBackground;
+        }
+    }
+}
+
